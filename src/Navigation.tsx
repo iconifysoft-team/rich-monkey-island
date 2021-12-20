af609e360f5d3babfcac6451af583353cc24db10
bluehat <bluehat@192.168.1.17>
2021-12-20 09:56:20 -0600

    upgrade website

--- src/Navigation.tsx
+++ src/Navigation.tsx
@@ -8,7 +8,8 @@ import monkey02 from './assets/imgs/monkey02.jpeg';
 import monkey03 from './assets/imgs/monkey03.jpeg';
 import  Navbar  from 'react-bootstrap/Navbar';
 import Footer from './footer';
-import { BsTwitter, BsWallet } from 'react-icons/bs';
+import { BsTwitter, BsFacebook, BsDiscord } from "react-icons/bs";
+import team from './assets/imgs/team.webp';
 
 export const Navigation: FC = () => {
     const { wallet } = useWallet();
@@ -42,10 +43,10 @@ export const Navigation: FC = () => {
                <Nav.Link href="#action1">Home</Nav.Link>
                <Nav.Link href="#action2">Buy Rich Monkey</Nav.Link>
                <Nav.Link href="#action2">Rarity</Nav.Link>
-               <Nav.Link href="#action2">Roadmap</Nav.Link>
-               <Nav.Link href="#action2">Team</Nav.Link>
+               <Nav.Link href="#roadmap">Roadmap</Nav.Link>
+               <Nav.Link href="#team">Team</Nav.Link>
                <Nav.Link href="#action2">Provenance</Nav.Link>
-               <Nav.Link href="#action2">Members Only</Nav.Link>
+               <Nav.Link href="#members">Members Only</Nav.Link>
               {/* <NavDropdown title="Dropdown" id="offcanvasNavbarDropdown">
                  <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
                  <NavDropdown.Item href="#action4">Another action</NavDropdown.Item>
@@ -68,12 +69,29 @@ export const Navigation: FC = () => {
          </Navbar.Offcanvas>
        </Container>
      </Navbar>
-     <div className="panel-center">
-	          	<div >
-		            <h2 className="welcome">Welcome</h2>
-		            <h3 className="welcome">To The Rich Monkey Island</h3>
-	            </div>
-	</div>
+     <Container>
+	          	<section className='precious'>
+              <div id="app">
+  <div className="title">
+    <div className="title-inner">
+      <div className="cafe">
+        <div className="cafe-inner title-shine">Welcome</div>
+      </div>
+      <div className="mozart">
+        <div className="mozart-inner title-shine">To The Rich Monkey Island</div>
+      </div>
+      <div className='cafe'>
+        <div className="image">
+          
+ RMI NFTs is a collection of Unique 7,500 NFTs living forever on the solana network .
+ Your NFT will not be only a great piece of art but also utility for the future free mints of the potions and also an access to members-only benefits that will be revealed with roadmap activation
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+	            </section>
+	</Container>
      <Carousel fade>
      <Carousel.Item>
        <img
@@ -112,6 +130,42 @@ export const Navigation: FC = () => {
      </Carousel.Item>
    </Carousel>
 
+  <section className='roadmap-style'>
+  <a id="roadmap">
+  <h2 className='roadmap-title'>ROADMAP</h2>
+  <hr></hr>
+  <ol className='roadmap-list'>
+  <li>Minting 7.5k Rich Monkey NFTs in the public sale 0.65 each .</li>
+  <li>Free Minting 3000 vodoo potion NFTS for Rich monkey NFTs holders.</li>
+  <li>Minting 7.5k Rich Monkey Wife NFTs by using the voodoo potion NFTs.</li>
+  <li>Minting 15k Rich Monkey Kid NFTs by using the voodoo potion NFTs.</li>
+</ol>
+<h3 className='line line-text'>Following the activation of the Road Map, a new roadmap for the Magical Island will begin. </h3>
+ 
+  </a>
+  </section>
+  <section className="team-style">
+    <a id="team">
+    <h2 className='team-title'>TEAM</h2>
+  <hr></hr>
+
+<div className="profile-card">
+   <div className="img">
+      <img src={team} width="160px" height="155px" />
+   </div>
+   <div className="caption">
+     <h3>Richard Cyber</h3>
+     <p>Blockchain Developer</p>
+    <div className="social-links">
+      <a href="#"><i><BsTwitter /></i></a>
+      <a href="#"><i><BsFacebook /></i></a>
+      <a href="#"><i><BsDiscord /></i></a>
+    </div>
+  </div>
+</div>
+    </a>
+  </section>
+  
    <Footer /> 
     </Container>
   
